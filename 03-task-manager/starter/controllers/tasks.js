84907480b7b2829771fe7de3d44a5b889f2801cf
shalom tubul <shalomtubul30@gmail.com>
2025-03-03 08:18:59 +0200

    Implement CRUD operations for tasks in the task manager

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,7 +1,23 @@
 const getAllTasks = (req, res) => {
   res.send('all items');
 };
+const getTask = (req, res) => {
+  res.send('get a Single Task');
+};
+const createTask = (req, res) => {
+  res.send('create Task');
+};
+const deleteTask = (req, res) => {
+  res.send('delete Task');
+};
+const updateTask = (req, res) => {
+  res.send('update Task');
+};
 
 module.exports = {
   getAllTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask
 };
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTasks } = require('../controllers/tasks.js');
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask,
+} = require('../controllers/tasks.js');
 
-router.route('/').get(getAllTasks);
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).delete(deleteTask).patch(updateTask);
 
 module.exports = router;

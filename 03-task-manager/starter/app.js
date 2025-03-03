5944141bc72319e1997690cfda013ef569e826a3
shalom tubul <shalomtubul30@gmail.com>
2025-03-03 07:43:01 +0200

    set up task manager app with Express and routing

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const tasks = require('./routes/tasks');
+
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});

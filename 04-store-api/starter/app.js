77d62abc48822bcbea6e517475e53da7ff9d9670
shalom tubul <shalomtubul30@gmail.com>
2025-03-10 17:02:37 +0200

    Remove outdated image, update info.txt, and refactor app.js to include middleware and improve server startup

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+//middlewares
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      `listaing on port ${port}`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

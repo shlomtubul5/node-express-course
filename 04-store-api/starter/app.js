80b8f2f9ab56275e275deefb10437f31cda24bff
shalom tubul <shalomtubul30@gmail.com>
2025-03-13 07:34:52 +0200

    Refactor product API: add product routes, update dependencies, and enhance database connection

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
-
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
+require('express-async-errors');
 
 //middlewares
 app.use(express.json());
@@ -15,6 +16,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
+app.use('/api/v1/products', productsRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
@@ -22,8 +25,11 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
+    console.log('DB is connected ');
+
     app.listen(port, () => {
-      `listaing on port ${port}`;
+      console.log(`Listening on port ${port}`);
     });
   } catch (error) {
     console.log(error);

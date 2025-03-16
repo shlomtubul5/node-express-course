eea800c641b7e03123ec51e91964ef842da87e5b
shalom tubul <shalomtubul30@gmail.com>
2025-03-16 14:52:09 +0200

    Enhance product API: implement dynamic product retrieval and populate database with initial data; update info.txt with new video links

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,21 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-//  throw new Error('testing async error')
-  res.status(200).json({ msg: 'product testing route' });
+  //  throw new Error('testing async error')
+  const products = await Product.find({ name: 'vase table' });
+  res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'product route' });
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  const products = await Product.find(queryObject);
+  console.log(queryObject);
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {

71506ce3efdbfd5d0c1c34d10ab56af803a803a4
shalom tubul <shalomtubul30@gmail.com>
2025-03-18 12:28:27 +0200

    Enhance product retrieval: implement dynamic querying with sorting options and update info.txt with new video links

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,18 +2,38 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
   //  throw new Error('testing async error')
-  const products = await Product.find({ name: 'vase table' });
-  res.status(200).json({ products });
+  const search = 'ab';
+  const products = await Product.find({}).sort('-name price');
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
-  const products = await Product.find(queryObject);
+
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
   console.log(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    console.log(sort);
+
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+    //products = products.sort();
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
 };
